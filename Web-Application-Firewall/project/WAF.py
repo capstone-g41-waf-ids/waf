4a690a5640bc680d8cf5d2f55ea8d9b180652bb9
NoNameMan <royston02.rj@gmail.com>
2022-08-23 20:06:46 +1000

    added IPv4, ICMP, TCP unpacker

--- Web-Application-Firewall/project/WAF.py
+++ Web-Application-Firewall/project/WAF.py
@@ -2,6 +2,7 @@ import socket
 import struct
 from threading import Thread
 
+
 HOST = socket.gethostbyname(socket.gethostname())#gets machines IP address
 
 def sniff_packets():
@@ -12,7 +13,11 @@ def sniff_packets():
         raw_data, address = Server.recvfrom(65535)#Biggest buffer size available is 65535 
         dest_mac, src_mac, protocol, data = unpack_frame(raw_data)
         print('\nPacket Frame:')
-        print('IP address: {}, Destination: {}, Source: {}, Protocol: {}'.format(address,dest_mac,src_mac,protocol))
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac,src_mac,protocol))
+        version, header_length, time_to_live, protocol_2, src, target, IPv4_data = unpack_IPv4(data)
+        print('IP address: {}'.format(src))
+
+
 
 def unpack_frame(data):
     dest_mac, src_mac, protocol = struct.unpack('! 6s 6s H', data[:14])
@@ -21,11 +26,38 @@ def unpack_frame(data):
 def get_mac_address(btyes_stream):
     mac_address = map('{:02x}'.format, btyes_stream)
     return ':'.join(mac_address).upper()
+
+def IP_translate(unformatted_address):
+    return '.'.join(map(str, unformatted_address))
+
+#upacks IPv4 packet
+def unpack_IPv4(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    time_to_live, protocol, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, time_to_live, protocol, IP_translate(src), IP_translate(target), data[header_length:]
+
+def unpack_ICMP(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[:4]
+
+def unpack_tcp(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('!H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ark = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ark, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
 try:
     print('Program started')
     if __name__ == "__main__":  
-        Sniffer_thread1 = Thread(target=sniff_packets)#Create Thread
-        Sniffer_thread1.start()#Start Thread
+        Sniffer_thread = Thread(target=sniff_packets)#Create Thread
+        Sniffer_thread.start()#Start Thread
 
 except KeyboardInterrupt:
     print('GOOOD BYE - KeyboardInterrupt')
