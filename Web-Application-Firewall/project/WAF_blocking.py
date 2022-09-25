a376257f90a9fba881d353295a6445d111346c18
orangeblossomest <43899498+orangeblossomest@users.noreply.github.com>
2022-09-26 01:03:48 +1000

    traversal and file inclusion regex done

--- Web-Application-Firewall/project/WAF_blocking.py
+++ Web-Application-Firewall/project/WAF_blocking.py
@@ -1,13 +1,13 @@
 """file name/structure not finalised. will be consolidated/adjusted"""
 
-import ipaddress
+import re
+
+CASE_INSENSITIVE = "(?i)"
 
 #ELLEN ADDITION http methods
 #check if method is allowed
 # https://owasp.org/www-project-web-security-testing-guide/v41/4-Web_Application_Security_Testing/02-Configuration_and_Deployment_Management_Testing/06-Test_HTTP_Methods
-bad_methods = ['PUT', 'MERGE', 'PATCH', 'DELETE', 'CONNECT', 'TRACE']
-if packet.method in bad_methods, block packet
-if packet.x_http_method_override <> "", block packet
+bad_methods = "(PUT|MERGE|PATCH|DELETE|CONNECT|TRACE)"
 
 #Special Element Injections
 # ellen note - this seems super general , probably covered by other items?
@@ -15,32 +15,58 @@ if packet.x_http_method_override <> "", block packet
 #Command and OS Command Injections
 #just check if any special chars are in packet url or body text
 #https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/07-Input_Validation_Testing/12-Testing_for_Command_Injection
-special_chars = ['!', '\"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}', '~', '–', '‘']
-#need to work out how to parse these properly so it wont block legit urls - look into it more
-if [i for i in special_chars if i in packet.url], block #obvs not sufficient
-if [i for i in special_chars if i in packet.body], block
+#special_chars = ['!', '\"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}', '~', '–', '‘']
 
-#XXS Attempts
+#XSS Attempts
+#reflected xss
+#xss = ["<", ">", "javascript", ".js", "<script", "script>", "alert", "bot", "\.", "\\", "\/", "//"]
+
+#stored xss
 
 #SQL Injection
+# stored in database
+sqli = "(?i)"
+#sqli = ["insert", "select", "union", "alter", "create", "delete", "drop", "execute", "exec", "merge", "update", "*", "<", ">", "1=1", "'1'", "--", "1'1", " = ", ";" ]
+# for < > need to filter out expected tags
 
 #XML and CRLF Injections
+xml = ""
 
 # Status Code Injections
+status_code_injections = ""
 
-#PHP File inclusions
+# path traversal
+# in URL
+traversal = "(?i)((((\.|%(25)?2e)+(\\|%(25)?5C)*(\\?\/|%((25)?2f|c0|af))+)+)|(\.|%(25)?2e){2,}|\\+)"
+
+#IN URL
+lfi = "(?i)((.+((http|php|zip|data|expect):))|(%00)+)"
+#["<not https or http>:", "file=", "php:", "zip:", "data:", "expect:" "<directory traversal>", "%00"]
+
+#PHP File inclusions (literally just lfi)
+php_lfi = "(?i)((\.php)+)"
+
+#remote file includsion
+# IN URL
+rfi = ["file"]
+## ALSO = http more than once, <- captured in lfi blacklist
 
 #Resource injection
+resource_injection = ""
 
 #HTTP Response splitting
+http_response_splitting = ""
 
 #Xpath and xquery injections
+xpath_xquery_injection = ""
 
 #scripted http headers and expression language injections
+header = "<script>"
+if [i for i in header_scripting if i in packet.header.useragent], block
 
 #encoding injections
 
-#improper local and remote file inclusions
+
 
 #http request smuggling
 
@@ -48,5 +74,13 @@ if [i for i in special_chars if i in packet.body], block
 
 #ip parsing
 
+blacklists = [xss, sqli, traversal, lfi, php_lfi, rfi, ETC] #array of blacklists
 
+#can/should be further focused on specific parts of the packet... or regex can be specified (e.g. COOKIE:)
 def check(pkt):
+    output = ""
+    for i in blacklists:
+        output = re.search(i, pkt) #check the packet against each of the blacklists
+        if output is not None: # if there is a regex match
+            return "403" #return Forbidden status code
+    return "200" #return OK status code
