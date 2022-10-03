e6880ceee299491ea248f4ad7c88064e663b1dab
NoNameMan <royston02.rj@gmail.com>
2022-10-03 21:27:41 +1100

    using tail now in a subprocess for simpler code

--- Web-Application-Firewall/logger_mk2.py
+++ Web-Application-Firewall/logger_mk2.py
@@ -1,33 +1,22 @@
 import os
 import time
-import pymongo
+import subprocess
+import select
+#import pymongo
 
 connstring = os.environ['MONGODB_CONNSTRING'] #from container env
 myclient = pymongo.MongoClient(connstring) #connect to mongo
 mydb = myclient["database"]
 mycol = mydb["WAFLogs"]
-response_old = ''
-old_result = ''
+
+
+f = subprocess.Popen(['tail','-F','var/log/nginx/host.access.log'],\
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+p = select.poll()
+p.register(f.stdout)
+
 while True:
-    response = os.popen(f"cat var/log/nginx/host.access.log").read()
-    if response != response_old:
-        result = ''
-                
-        for i in range(len(response)):
-            #use a slice rather than index in case one string longer than other
-            letter1=response_old[i:i+1]
-            letter2=response[i:i+1]
-            #create string with differences
-            if letter1 != letter2:
-                result+=letter2
-
-        if result != old_result:
-            print (result)
-            x = mycol.insert_one(result)
-            old_result = result
-
-        response_old = response
-        
-
-    response = ''
+    if p.poll(1):
+        print(f.stdout.readline())
+        x = mycol.insert_one(f.stdout.readline())
     time.sleep(5)
