0420883e07378d64bf643d794e0f4c4f052bfe35
orangeblossomest <43899498+orangeblossomest@users.noreply.github.com>
2022-10-08 13:52:50 +1100

    cleanup
    - remove portal\nginx.conf
    - remove unecessary string from db import.sh
    +rename container docker-compose
    + re-add ../portal/www: and ports to docker-compose

--- database/init/import.sh
+++ database/init/import.sh
@@ -1,4 +1,4 @@
-mongoimport --db database --collection WAFLogs --type json --file "../seed/WAFLogs.json" --jsonArray
+mongoimport --db database --collection WAFLogs --file "../seed/WAFLogs.json" --jsonArray
 mongoimport --db database --collection CountryBlacklist --file "../seed/CountryBlacklist.json" --jsonArray
 mongoimport --db database --collection HTTPTypes --file "../seed/HTTPTypes.json" --jsonArray
 mongoimport --db database --collection IPBlacklist --file "../seed/IPBlacklist.json" --jsonArray 
