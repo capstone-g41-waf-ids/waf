5a885f8a8f6c3671895a04fdaf71d9b7fb4a0086
NoNameMan <royston02.rj@gmail.com>
2022-10-10 12:11:12 +1100

    logger, pymongo, search function added

--- web_application_firewall/flask_webserver.py
+++ web_application_firewall/flask_webserver.py
@@ -1,8 +1,18 @@
+import json
 import os
-
+import time
+import subprocess
+import select
+import pymongo
+from threading import Thread
 from flask import Flask, render_template, Response, request
 
 app = Flask(__name__)
+
+connstring = os.environ['MONGODB_CONNSTRING'] #from container env
+myclient = pymongo.MongoClient(connstring) #connect to mongo
+mydb = myclient["database"]
+
 
 @app.route('/')
 def index():
@@ -34,12 +44,39 @@ def forgotpass():
 
 @app.route('/logsearch.html')
 def logsearch():
-    return render_template('logsearch.html')
+    mycol = mydb["WAFLogs"]
+    x = mycol.find()
+ 
+    return render_template('logsearch.html', results = x)
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_data = request.form['searched']
+    search_field = request.form['field']
+    mycol = mydb["WAFLogs"]
+    myquery = {search_field : search_data}
+    x = mycol.find(myquery)
+    return render_template('search.html', results = x)
+
 
 #@app.route('/')
 #def e():
 #   return render_template('')
 
+def logger():
+    mycol = mydb["WAFLogs"]
+    f = subprocess.Popen(['tail','-F','var/log/nginx/host.access.log'],\
+            stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    p = select.poll()
+    p.register(f.stdout)
 
+    while True:
+        if p.poll(1):
+            print(f.stdout.readline())
+            mycol.insert_one(json.loads(f.stdout.readline()))
+        time.sleep(5)
+    
 if __name__ == '__main__':
+    logger = Thread(target=logger)
+    logger.start()
     app.run(host='172.2.2.4',port = 30, debug = True)
