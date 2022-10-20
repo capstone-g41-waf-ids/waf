f6ef189f5b638004e89f6c95e9f6dcad2f58dbe7
orangeblossomest <43899498+orangeblossomest@users.noreply.github.com>
2022-10-20 21:31:32 +1100

    rename app folder

--- web_application_firewall/docker-entrypoint.sh
+++ web_application_firewall/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 
 service nginx start
 pip install pyopenssl
-#uwsgi app/wsgi.ini
-python3 app/flask_webserver.py #uncommented to allow functionality during testing
+uwsgi portal/wsgi.ini
+#python3 portal/flask_webserver.py #uncomment to allow functionality during testing
