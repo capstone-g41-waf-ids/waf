03a07beb92660a915ce09cdcce17d5a7b56e0ad6
orangeblossomest <43899498+orangeblossomest@users.noreply.github.com>
2022-10-26 01:30:20 +1100

    added custom rules functionality + general website improvements
    + users can now add custom rules
    + general website cleanup
    + login is centred
    + removed white block at bottom of edit user
    + user current password is now "password" type
    +applied redirects where relevant to website
    + search results now return in order of time
    + removed redundant functionality
    + updated naming conventions
    + reorganised flask file to make more sense
    + got rid of old header php file
    + mild css updates in log page
    + logs now show modsec message NEEDS TO BE SIMPLIFIED
    + added page title to nav (final name TBC)
    + updated nav
    + temp disabled log to file for wsgi for testing - should be re-enabled in main

--- web_application_firewall/portal/flask_webserver.py
+++ web_application_firewall/portal/flask_webserver.py
@@ -26,183 +26,14 @@ def index():
 @app.route('/check_login', methods=['POST'])
 def check_login():
     if "user" in session:
-        return render_template('/logsearch.html')
+        return redirect('/logsearch')
     else:
         username_local = request.form['uname']
         password_local = request.form['pword']
-        myquery = {"username": username_local, "password": hash(password_local)}
-        user = mydb.UserAccounts.find_one(myquery)
-        if user is not None:
+        account = mydb.UserAccounts.find_one({"username": username_local, "password": hash_pword(password_local)})
+        if account is not None:
             session["user"] = username_local
-            return render_template('/logsearch.html', results=get_access_logs(), results2=get_audit_logs())
-        return redirect('/login')
-
-
-@app.route('/serverstatus')
-def serverstatus():
-    if "user" in session:
-        response = os.popen(f"curl --max-time 2 -I http://webgoat:8080/WebGoat").read() #HARDCODED WEBGOAT
-        if "HTTP/1.1 302 Found" in response:
-            return render_template('serverstatus.html', status="Active", emote="&#128578")
-        else:
-            return render_template('serverstatus.html', status="Inactive", emote="&#128577")
-    else:
-        return redirect('/login')
-
-
-@app.route('/edituser')
-def edituser():
-    if "user" in session:
-        return render_template('edituser.html', result=session["user"])
-    else:
-        return redirect('/login')
-
-def hash(var):
-    return hashlib.md5(var.encode('utf-8')).hexdigest()
-
-@app.route('/editcurrentuser', methods=['POST'])
-def editcurrentuser():
-    if "user" in session:
-        mycol = mydb["UserAccounts"]
-
-        old_pword = hash(request.form['current_pword'])
-        new_pword = hash(request.form['pword'])
-        old_user = {"username": session["user"], "password": old_pword}
-
-        result = mycol.update_one(old_user, {"$set": {"password": new_pword}})
-
-        if result.modified_count > 0:
-            message = "SUCCESS! User updated successfully."
-        message = "ERROR! User did not update. Please try again."
-
-        return render_template('/updateuser.html', message=message)
-    else:
-        return redirect('/login')
-
-
-def get_GeoBlacklist_options():
-    if "user" in session:
-        with open("../country_codes") as json_file:
-            return json.load(json_file)
-    else:
-        return redirect('/login')
-
-
-def get_blacklist():
-    if "user" in session:
-        return mydb.IPBlacklist.find()
-    else:
-        return redirect('/login')
-
-
-def get_GeoBlacklist():
-    if "user" in session:
-        return mydb.GEOIP_blacklist.find()
-    else:
-        return redirect('/login')
-
-
-@app.route('/firewall')
-def firewall():
-    if "user" in session:
-        return render_template('firewall.html', results_1=get_blacklist(), results_2=get_GeoBlacklist(),
-                               results_3=get_GeoBlacklist_options())
-    else:
-        return redirect('/login')
-
-
-@app.route('/blacklistIP', methods=['POST'])
-def blacklistIP():
-    if "user" in session:
-        ip = request.form['ip_blacked']
-        myquery = {'ip': ip}
-        mydb.IPBlacklist.replace_one(myquery, myquery, upsert=True)
-        update_blacklist_file()
-        return render_template('firewall.html', results_1=get_blacklist(), results_2=get_GeoBlacklist(),
-                               results_3=get_GeoBlacklist_options())
-    else:
-        return redirect('/login')
-
-
-@app.route('/blacklistGEO', methods=['POST'])
-def blacklistGEO():
-    if "user" in session:
-        geolocation = request.form['geoip_blacked']
-        myquery = {"country_code": geolocation}
-        mydb.GEOIP_blacklist.replace_one(myquery, myquery, upsert=True)
-        update_geoIP_file()
-        return render_template('firewall.html', results_1=get_blacklist(), results_2=get_GeoBlacklist(),
-                               results_3=get_GeoBlacklist_options())
-    else:
-        return redirect('/login')
-
-
-@app.route('/deleteIP', methods=['POST'])
-def deleteIP():
-    if "user" in session:
-        delete_ip = request.form['deleteIP']
-        mydb.IPBlacklist.delete_one({"ip": delete_ip})
-        update_blacklist_file()
-        return render_template('firewall.html', results_1=get_blacklist(), results_2=get_GeoBlacklist(),
-                               results_3=get_GeoBlacklist_options())
-    else:
-        return redirect('/login')
-
-
-@app.route('/delete_geo', methods=['POST'])
-def delete_geo():
-    if "user" in session:
-        delete_geo = request.form['delete_geo']
-        mydb.GEOIP_blacklist.delete_one({"country_code": delete_geo})
-        update_geoIP_file()
-        return render_template('firewall.html', results_1=get_blacklist(), results_2=get_GeoBlacklist(),
-                               results_3=get_GeoBlacklist_options())
-    else:
-        return redirect('/login')
-
-
-@app.route('/logsearch')
-def logsearch():
-    if "user" in session:
-        return render_template('logsearch.html', results=get_access_logs(), results2=get_audit_logs())
-    else:
-        return redirect('/login')
-
-
-def get_access_logs():
-    if "user" in session:
-        return mydb.WAFLogs.find().sort("time", -1)
-    else:
-        return redirect('/login')
-
-
-def get_audit_logs():
-    if "user" in session:
-        return mydb.modsec_audit_logs.find().sort("time", -1)
-    else:
-        return redirect('/login')
-
-
-@app.route('/search', methods=['POST'])
-def search():
-    if "user" in session:
-        search_data = request.form['searched']
-        search_field = request.form['field']
-        myquery = {search_field: {"$regex": search_data}}
-        result = mydb.WAFLogs.find(myquery)
-        return render_template('logsearch.html', results=result, results2=get_audit_logs())
-    else:
-        return redirect('/login')
-
-
-@app.route('/auditlogsearch', methods=['POST'])
-def auditlogsearch():
-    if "user" in session:
-        search_data = request.form['searched']
-        myquery = {"log": {"$regex": search_data}}
-        result = mydb.modsec_audit_logs.find(myquery)
-        return render_template('logsearch.html', results=get_access_logs(), results2=result)
-    else:
+            return redirect('/logsearch')
         return redirect('/login')
 
 
@@ -215,9 +46,209 @@ def logout():
         return redirect('/login')
 
 
+@app.route('/edituser')
+def edituser():
+    if "user" in session:
+        return render_template('edituser.html', result=session["user"])
+    return redirect('/login')
+
+
+@app.route('/editcurrentuser', methods=['POST'])
+def editcurrentuser():
+    if "user" in session:
+        mycol = mydb["UserAccounts"]
+
+        old_pword = hash_pword(request.form['current_pword'])
+        new_pword = hash_pword(request.form['pword'])
+        old_user = {"username": session["user"], "password": old_pword}
+
+        result = mycol.update_one(old_user, {"$set": {"password": new_pword}})
+
+        if result.modified_count > 0:
+            return render_template('/updateuser.html', message="SUCCESS! User updated successfully.")
+        return render_template('/updateuser.html', message="ERROR! User did not update. Please try again.")
+    else:
+        return redirect('/login')
+
+
+@app.route('/serverstatus')
+def serverstatus():
+    if "user" in session:
+        response = os.popen(f"curl --max-time 2 -I http://webgoat:8080/WebGoat").read()  # HARDCODED WEBGOAT MUST FIX
+        if "HTTP/1.1 302 Found" in response:
+            return render_template('serverstatus.html', status="Active", emote="\U0001F642")
+        return render_template('serverstatus.html', status="Inactive", emote="\U0001F641")
+    else:
+        return redirect('/login')
+
+
+@app.route('/firewall')
+def firewall():
+    if "user" in session:
+        return render_template('firewall.html', ip_blacklist=get_blacklist(), geo_blacklist=get_geoblacklist(),
+                               geo_list=get_geoblacklist_options(), rule_list=get_custom_rules())
+    else:
+        return redirect('/login')
+
+
+def get_blacklist():
+    if "user" in session:
+        return mydb.IPBlacklist.find()
+    return redirect('/login')
+
+
+@app.route('/blacklist_ip', methods=['POST'])
+def blacklist_ip():
+    if "user" in session:
+        ip = request.form['block_ip']
+        myquery = {'ip': ip}
+        mydb.IPBlacklist.replace_one(myquery, myquery, upsert=True)
+        update_blacklist_file()
+        return redirect('/firewall')
+    else:
+        return redirect('/login')
+
+
+@app.route('/delete_ip', methods=['POST'])
+def delete_ip():
+    if "user" in session:
+        ip = request.form['delete_ip']
+        mydb.IPBlacklist.delete_one({"ip": ip})
+        update_blacklist_file()
+        return redirect('/firewall')
+    else:
+        return redirect('/login')
+
+
+def update_blacklist_file():
+    f = open("/etc/nginx/blacklist", "w")
+    x = mydb.IPBlacklist.find()
+    for data in x:
+        if data["ip"] is not None:
+            f.write("deny " + data["ip"] + ";\n")
+    f.close()
+    os.system('service nginx reload')
+
+
+def get_geoblacklist():
+    if "user" in session:
+        return mydb.GEOIP_blacklist.find()
+    return redirect('/login')
+
+
+def get_geoblacklist_options():
+    if "user" in session:
+        with open("../country_codes") as json_file:
+            return json.load(json_file)
+    return redirect('/login')
+
+
+@app.route('/blacklist_geo', methods=['POST'])
+def blacklist_geo():
+    if "user" in session:
+        geolocation = request.form['block_geo']
+        myquery = {"country_code": geolocation}
+        mydb.GEOIP_blacklist.replace_one(myquery, myquery, upsert=True)
+        update_geo_file()
+        return redirect('/firewall')
+    else:
+        return redirect('/login')
+
+
+@app.route('/delete_geo', methods=['POST'])
+def delete_geo():
+    if "user" in session:
+        geo = request.form['delete_geo']
+        mydb.GEOIP_blacklist.delete_one({"country_code": geo})
+        update_geo_file()
+        return redirect('/firewall')
+    else:
+        return redirect('/login')
+
+
+def update_geo_file():
+    f = open("/etc/nginx/GEOIP_blacklist", "w")
+    x = mydb.GEOIP_blacklist.find()
+    for data in x:
+        if data["country_code"] is not None:
+            f.write(data["country_code"] + " no;\n")
+    f.close()
+    os.system('service nginx reload')
+
+
+def get_custom_rules():
+    if "user" in session:
+        return mydb.ModsecCustomRules.find()
+    return redirect('/login')
+
+
+@app.route('/add_rule', methods=['POST'])
+def add_rule():
+    if "user" in session:
+        rule = request.form['add_rule']
+        myquery = {"rule": rule}
+        mydb.ModsecCustomRules.replace_one(myquery, myquery, upsert=True)
+        update_rule_file()
+        return redirect('/firewall')
+    else:
+        return redirect('/login')
+
+
+@app.route('/delete_rule', methods=['POST'])
+def delete_rule():
+    if "user" in session:
+        rule = request.form['delete_rule']
+        mydb.ModsecCustomRules.delete_one({"rule": rule})
+        update_rule_file()
+        return redirect('/firewall')
+    else:
+        return redirect('/login')
+
+
+def update_rule_file():
+    f = open("/etc/modsecurity.d/custom_rules.conf", "w")
+    x = mydb.ModsecCustomRules.find()
+    for data in x:
+        if data["rule"] is not None:
+            f.write(data["rule"] + "\n")
+    f.close()
+    os.system('service nginx reload')
+
+
+@app.route('/logsearch')
+def logsearch():
+    if "user" in session:
+        return render_template('logsearch.html', results=get_access_logs())
+    return redirect('/login')
+
+
+def get_access_logs():
+    if "user" in session:
+        return mydb.WAFLogs.find().sort("time", -1)
+    return redirect('/login')
+
+
+def get_audit_logs():
+    if "user" in session:
+        return mydb.modsec_audit_logs.find().sort("time", -1)
+    return redirect('/login')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    if "user" in session:
+        search_data = request.form['searched']
+        search_field = request.form['field']
+        myquery = {search_field: {"$regex": search_data}}
+        result = mydb.WAFLogs.find(myquery).sort("time", -1)
+        return render_template('logsearch.html', results=result)
+    else:
+        return redirect('/login')
+
+
 @uwsgidecorators.postfork
 @uwsgidecorators.thread
-def access_logger():
+def nginx_logger():
     f = subprocess.Popen(['tail', '-F', '/var/log/nginx/host.access.log'], stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     p = select.poll()
@@ -231,7 +262,7 @@ def access_logger():
 
 @uwsgidecorators.postfork
 @uwsgidecorators.thread
-def audit_logger():
+def modsec_logger():
     f = subprocess.Popen(['tail', '-F', '/var/log/nginx/modsec_audit_log.log'], stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     p = select.poll()
@@ -239,37 +270,21 @@ def audit_logger():
     while True:
         if p.poll(1):
             log = json.loads(f.stdout.readline())
-            mydb.WAFLogs.update_one({'request_id': log['transaction']['unique_id']},
-                             {'$set': {'messages': log['transaction']['messages']}}, upsert=True)
+            mydb.WAFLogs.update_one({'request_id': log['transaction']['unique_id']}, {'$set': {'messages': log['transaction']['messages']}}, upsert=True)
         time.sleep(5)
 
 
-def update_blacklist_file():
-    f = open("/etc/nginx/blacklist", "w")
-    x = mydb.IPBlacklist.find()
-    for data in x:
-        if data["ip"] is not None:
-            f.write("deny " + data["ip"] + ";\n")
-    f.close()
-    os.system('service nginx reload')
-
-
-def update_geoIP_file():
-    f = open("/etc/nginx/GEOIP_blacklist", "w")
-    x = mydb.GEOIP_blacklist.find()
-    for data in x:
-        if data["country_code"] is not None:
-            f.write(data["country_code"] + " no;\n")
-    f.close()
-    os.system('service nginx reload')
+def hash_pword(var):
+    return hashlib.md5(var.encode('utf-8')).hexdigest()
 
 
 if __name__ == '__main__':
     update_blacklist_file()
-    update_geoIP_file()
-    access_logger = Thread(target=access_logger)
-    access_logger.start()
-    audit_logger = Thread(target=audit_logger)
-    audit_logger.start()
+    update_geo_file()
+    update_rule_file()
+    nginx_logger = Thread(target=nginx_logger)
+    nginx_logger.start()
+    modsec_logger = Thread(target=modsec_logger)
+    modsec_logger.start()
     context = ('/etc/nginx/ssl/secret.crt', '/etc/nginx/ssl/secret.key')
     app.run(host='172.2.2.4', port=30, debug=True, ssl_context=context)
